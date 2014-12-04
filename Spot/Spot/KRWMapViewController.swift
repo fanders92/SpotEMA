046223691b042ed7cb1d9c108b4c817470f2ee5d
Kw <Kw@Kw.local>
2014-12-05 00:03:00 +0100

    Added stuff

--- Spot/Spot/KRWMapViewController.swift
+++ Spot/Spot/KRWMapViewController.swift
@@ -21,6 +21,7 @@ class KRWMapViewController: UIViewController, CLLocationManagerDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         if (CLLocationManager.locationServicesEnabled()) {
+            // Set current ViewController to the delegate
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.requestWhenInUseAuthorization()
@@ -38,7 +39,7 @@ class KRWMapViewController: UIViewController, CLLocationManagerDelegate {
     // MARK: - CoreLocation Delegate Methods
     func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
         locationManager.stopUpdatingLocation()
-        //removeLoadingView()
+        //removeLoadingView() // ToDo find out what that means ????
         if ((error) != nil) {
             print(error)
         }
@@ -47,19 +48,24 @@ class KRWMapViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         var locationArray = locations as NSArray
         var locationObj = locationArray.lastObject as CLLocation
+        //Coordinations od the current position (gps)
         var coord = locationObj.coordinate
+        
         println(coord.latitude)
         println(coord.longitude)
+        // Calculate the center
         let center = CLLocationCoordinate2D(latitude: coord.latitude, longitude: coord.longitude)
+        // Calculate the region which is shown by themap
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        
+        // Set the Maps view to the region
         self.mapView.setRegion(region, animated: true)
+        
     }
     
     func locationManager(manager: CLLocationManager!,
         didChangeAuthorizationStatus status: CLAuthorizationStatus) {
             var shouldIAllow = false
-            
+            // status of authorization
             switch status {
             case CLAuthorizationStatus.Restricted:
                 locationStatus = "Restricted Access to location"
